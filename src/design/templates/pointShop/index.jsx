26cf0e28b3736095341d61cad922d35c4b8df15d
wangduo3 <wangduo3@hisense.com>
2019-07-31 17:21:06 +0800

    积分商城小程序未实现提示

--- src/design/templates/pointShop/index.jsx
+++ src/design/templates/pointShop/index.jsx
@@ -5,17 +5,22 @@ import templateShopImg from '@/static/images/pointShop.jpg'
 import './index.less'
 import { Icon } from 'antd'
 
-class Title extends PureComponent {
+class PointShop extends PureComponent {
+
+  renderTip = () => {
+    return (
+      <div className='tip'>小程序端尚未实现积分商城,目前该项只对app、h5生效</div>
+    )
+  }
+
   render() {
-    const { componentStyle, data } = this.props
-    const moreStyle = classnames('right-content', {
-      hide: !data.moreSwitch
-    })
+    const { componentStyle, data, component } = this.props
     return (
       <div className='x-template-point-shop' style={getStyles(componentStyle, ['margin'])}>
         <img src={templateShopImg} alt="积分商城" />
+        {component && this.renderTip()}
       </div>
     )
   }
 }
-export default Title
+export default PointShop
