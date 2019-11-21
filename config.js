e0a1da0d1c564f5a2c2f273f752b3383ce752e09
wangduo3 <wangduo3@hisense.com>
2019-11-21 16:45:21 +0800

    revert config.js

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
 /**
  * 常用配置: 端口, MySQL
  */
-
+const isDev = process.env.NODE_ENV === 'development'
+const port = isDev ? 3001 : 3000 // 主服务启动端口
 module.exports = {
-  port: 3001, // 主服务启动端口
+  port,
   mysql: {
     host: process.env.HISENSE_MYSQL_HOST || '127.0.0.1',
     port: process.env.HISENSE_MYSQL_PORT || 3306,
     username: process.env.HISENSE_MYSQL_USERNAME || 'root',
-    password: process.env.HISENSE_MYSQL_PASSWORD || 'anywhere',
+    password: process.env.HISENSE_MYSQL_PASSWORD || 'root',
     database: process.env.HISENSE_MYSQL_DATABASE || 'hisense',
+    logging: isDev,
   },
   webpack: {
     publicPath: '/',
